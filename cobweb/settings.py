62e54f426980ab42999e92466ed441c3b185ce43
Vincent Pham <vpham@quantcast.com>
2017-07-22 10:53:25 +0100

    Turn off proxy

--- cobweb/settings.py
+++ cobweb/settings.py
@@ -56,8 +56,8 @@ COOKIES_ENABLED=False
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
     'cobweb.middlewares.random_useragent.RandomUserAgentMiddleware': 400,
-    'cobweb.middlewares.random_proxy.RandomProxyMiddleware': 600,
-    'scrapy.downloadermiddlewares.retry': None
+    #'cobweb.middlewares.random_proxy.RandomProxyMiddleware': 600,
+    #'scrapy.downloadermiddlewares.retry': None
 }
 
 # Configure item pipelines
